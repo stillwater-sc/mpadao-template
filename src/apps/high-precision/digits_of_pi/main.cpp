040cd91b46c8ccc89166795121270f6eb6116f86
Ravenwater <theo@stillwater-sc.com>
2026-03-05 13:23:24 -0500

    Enhancing the structure

---
+++ src/apps/high-precision/digits_of_pi/main.cpp
@@ -0,0 +1,29 @@
+/** ************************************************************************
+* example: skeleton for working with Universal
+*
+* @author:     Theodore Omtzigt
+* @date:       2023-01-04
+* @copyright:  Copyright (c) 2023 Stillwater Supercomputing, Inc.
+* @license:    MIT Open Source license
+*
+* This file is part of the MPADAO project
+* *************************************************************************
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+
+#include <universal/number/ereal/ereal.hpp>
+
+int main()
+try {
+	using namespace sw::universal;
+
+	// calculate the digits of PI
+
+	return EXIT_SUCCESS;
+}
+catch(const char* msg) {
+	std::cerr << "Caught unexpected exception: " << msg << std::endl;
+	return EXIT_FAILURE;
+}
